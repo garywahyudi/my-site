74afdee8c621a8e87e732eaa5a573027da7f981b
garywahyudi <garywahyudi@users.noreply.github.com>
2025-08-25 16:04:29 +0000

    deploy: 7a02b44734e0d66dfff06d26d0fdeb25528e8df2

---
+++ _next/static/gKEJZ8NO_YeRUUEupMeF7/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/07ebf09ee754b023.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/my-site//_next/:path+",
+        "destination": "/my-site/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
